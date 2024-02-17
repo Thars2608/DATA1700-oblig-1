4f0b7804fcdea8700e19cca68664e1dfff015058
Thars2608 <143402345+Thars2608@users.noreply.github.com>
2024-02-17 21:59:56 +0100

    kjøp billett function og tomt array

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,16 @@
+let billetter = [];
+
+function kjopBillett(){
+    const filmInput = document.getElementById('filmer').value;
+    const navnInput = document.getElementById('navn').value;
+    const emailInput = document.getElementById('email').value;
+    const telefonInput = document.getElementById('telefon').value;
+}
+
+const billett = {
+    film: filmInput,
+    navn: navnInput,
+    epost: emailInput,
+    telefon: telefonInput
+};
+
